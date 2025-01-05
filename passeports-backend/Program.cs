3cd2513f562af50c6f9c3e9ce093f406f4f4ac8d
Pimer0 <zefun74@gmail.com>
2025-01-05 15:46:30 +0100

    deploy test

--- passeports-backend/Program.cs
+++ passeports-backend/Program.cs
@@ -93,7 +93,7 @@ public class Program
             logger.Error(ex, "Erreur lors de la connexion à la base de données");
         }
 
-        app.MapGroup("/passeports").MapPasseportEndpoints();
+        app.MapGroup("/passeportss").MapPasseportEndpoints();
 
         app.Run();
     }
